61b91bf42c38fd7f2683ada6a4d1d3c8930473bd
James Nesbitt <james.nesbitt@claranet.uk>
2020-05-12 15:15:35 +0100

    Output the DNS records to be used for the DNS validation (TF v0.11.14)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "certificate_arn" {
   description = "The ARN of the AWS ACM Certificate."
   value       = "${join("", coalescelist(aws_acm_certificate_validation.certificate_validation.*.certificate_arn, aws_acm_certificate.cert.*.arn))}"
 }
+
+output "dns_validation_records" {
+  description = "A list of DNS records required for DNS Validation of the ACM Certificate."
+  value       = "${aws_acm_certificate.cert.0.domain_validation_options}"
+}
