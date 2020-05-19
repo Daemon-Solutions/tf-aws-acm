1ccee220ccbd83a8a5acf9ba1251b8953d42ffc9
James Nesbitt <james.nesbitt@claranet.uk>
2020-05-19 16:02:45 +0100

    Fix the outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,12 @@
 output "certificate_arn" {
   description = "The ARN of the AWS ACM Certificate."
-  value = join(
-    "",
-    coalescelist(
+  value = aws_acm_certificate_validation.certificate_validation.*.certificate_arn != [] || aws_acm_certificate.cert.*.arn != [] ? coalescelist(
       aws_acm_certificate_validation.certificate_validation.*.certificate_arn,
       aws_acm_certificate.cert.*.arn,
-    ),
-  )
+    ) : []
 }
 
 output "dns_validation_records" {
   description = "A list of DNS records required for DNS Validation of the ACM Certificate."
-  value       = aws_acm_certificate.cert.0.domain_validation_options[*]
+  value       = length(aws_acm_certificate.cert) > 0 ? aws_acm_certificate.cert.0.domain_validation_options[*] : []
 }
